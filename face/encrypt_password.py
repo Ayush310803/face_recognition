c25d58198fd6afea192bff1d911b17018fb9dbad
Ayush310803 <Ayush.20b0121122@abes.ac.in>
2025-03-24 11:11:30 +0530

    added readme

--- face/encrypt_password.py
+++ face/encrypt_password.py
@@ -1,10 +1,8 @@
 from cryptography.fernet import Fernet
 
-# Generate encryption key (Run once and save it securely)
 key = Fernet.generate_key()
 print(f"Save this key securely: {key.decode()}")
 
-# Encrypt the password
 cipher_suite = Fernet(key)
-encrypted_password = cipher_suite.encrypt(b"saritasoni@7")
+encrypted_password = cipher_suite.encrypt(b"Ayush@310803")
 print(f"Encrypted Password: {encrypted_password.decode()}")
